4fa788eccd85b51098f27610674fa21839a42d80
josel120 <josegomez120@gmail.com>
2018-06-26 12:57:48 -0500

    se agrego dashboard y tabla por commandos angular 6

---
+++ main.js
@@ -0,0 +1,36 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron')
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  // Specify entry point
+  win.loadURL('http://localhost:4000');
+  //win.loadURL(`file://${__dirname}/dist/index.html`);
+
+  // Show dev tools
+  // Remove this line before distributing
+  win.webContents.openDevTools()
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { TablaArchivoComponent } from './components/tabla-archivo/tabla-archivo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TablaArchivoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
